d4a35cfbe8cc254a356d730f640f1364158bbce3
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-02-13 01:39:29 -0600

    update cdc service

--- Core/Src/Entry.cpp
+++ Core/Src/Entry.cpp
@@ -51,27 +51,33 @@ extern "C"
 
     const char responseText1[34] = "Initializing EmbeddedIOServices\n\r";
     _cdcService->Send((uint8_t*)responseText1, strlen(responseText1));
+    _cdcService->Flush();
     _embeddedIOServiceCollection.DigitalService = new Stm32HalDigitalService();
     _embeddedIOServiceCollection.AnalogService = new Stm32HalAnalogService();
     _embeddedIOServiceCollection.TimerService = new Stm32HalTimerService(TimerIndex::Index2);
     _embeddedIOServiceCollection.PwmService = new Stm32HalPwmService();
     const char responseText2[33] = "EmbeddedIOServices Initialized\n\r";
     _cdcService->Send((uint8_t*)responseText2, strlen(responseText2));
+    _cdcService->Flush();
 
     const char responseText3[26] = "Initializing EngineMain\n\r";
     _cdcService->Send((uint8_t*)responseText3, strlen(responseText3));
+    _cdcService->Flush();
 		size_t _configSize = 0;
     _engineMain = new EFIGenieMain(reinterpret_cast<void*>(&_config), _configSize, &_embeddedIOServiceCollection);
     const char responseText4[25] = "EngineMain Initialized\n\r";
     _cdcService->Send((uint8_t*)responseText4, strlen(responseText4));
+    _cdcService->Flush();
 
     _getVariableHandler = new CommunicationHandler_GetVariable(_cdcService, &_engineMain->SystemBus->Variables);
 
     const char responseText5[24] = "Setting Up EngineMain\n\r";
     _cdcService->Send((uint8_t*)responseText5, strlen(responseText5));
+    _cdcService->Flush();
     _engineMain->Setup();
     const char responseText6[19] = "EngineMain Setup\n\r";
     _cdcService->Send((uint8_t*)responseText6, strlen(responseText6));
+    _cdcService->Flush();
     loopTime = _engineMain->SystemBus->GetOrCreateVariable(250);
   }
   void Loop() 
@@ -79,6 +85,7 @@ extern "C"
     const tick_t now = _embeddedIOServiceCollection.TimerService->GetTick();
     loopTime->Set((float)(now-prev) / _embeddedIOServiceCollection.TimerService->GetTicksPerSecond());
     prev = now;
+    _cdcService->Flush();
     _engineMain->Loop();
   }
 }
