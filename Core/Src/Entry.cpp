8a0682bfe0c0c17deb2dafbe9727066cc74bb1e4
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-07-09 01:09:10 -0500

    update libs

--- Core/Src/Entry.cpp
+++ Core/Src/Entry.cpp
@@ -8,8 +8,10 @@
 #include "Variable.h"
 #include "CallBack.h"
 #include "Config.h"
+#include "CommunicationHandler_Prefix.h"
 #include "STM32HalCommunicationService_CDC.h"
 #include "CommunicationHandlers/CommunicationHandler_GetVariable.h"
+#include "stm32f4xx_hal_flash_ex.h"
 
 using namespace OperationArchitecture;
 using namespace EmbeddedIOServices;
@@ -22,13 +24,17 @@ extern char _config;
 
 extern "C"
 {
+  GeneratorMap<Variable> *_variableMap;
   uint8_t VariableBuff[9];
   uint32_t Commands[32];
   uint8_t CommandReadPointer = 0;
+  const char doneResponseText[10] = " (Done)\n\r";
+  const void *_metadata;
   bool secondCommand = false;
   EmbeddedIOServiceCollection _embeddedIOServiceCollection;
   STM32HalCommunicationService_CDC *_cdcService;
   CommunicationHandler_GetVariable *_getVariableHandler;
+  CommunicationHandler_Prefix *_prefixHandler;
   EFIGenieMain *_engineMain;
   Variable *loopTime;
   uint32_t prev;
@@ -48,7 +54,9 @@ extern "C"
     volatile size_t float_align = alignof(float);
     volatile size_t double_align = alignof(double);
 
+    _variableMap = new GeneratorMap<Variable>();
     _cdcService = new STM32HalCommunicationService_CDC();
+    _cdcService->RegisterHandler(_prefixHandler = new CommunicationHandler_Prefix());
 
     const char responseText1[34] = "Initializing EmbeddedIOServices\n\r";
     _cdcService->Send((uint8_t*)responseText1, strlen(responseText1));
@@ -65,14 +73,125 @@ extern "C"
     _cdcService->Send((uint8_t*)responseText3, strlen(responseText3));
     _cdcService->Flush();
 		size_t _configSize = 0;
-    _engineMain = new EFIGenieMain(reinterpret_cast<void*>(&_config), _configSize, &_embeddedIOServiceCollection);
+    _engineMain = new EFIGenieMain(reinterpret_cast<void*>(&_config), _configSize, &_embeddedIOServiceCollection, _variableMap);
     const char responseText4[25] = "EngineMain Initialized\n\r";
     _cdcService->Send((uint8_t*)responseText4, strlen(responseText4));
     _cdcService->Flush();
 
-    const void *metadata = Config::OffsetConfig(&_config, _configSize);
-    _getVariableHandler = new CommunicationHandler_GetVariable(_engineMain->VariableMap, metadata);
-    _cdcService->RegisterHandler(_getVariableHandler);
+    _metadata = Config::OffsetConfig(&_config, _configSize);
+    _getVariableHandler = new CommunicationHandler_GetVariable(_variableMap);
+    _prefixHandler->RegisterHandler(_getVariableHandler, "g", 1);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      const size_t minDataSize = sizeof(void *) + sizeof(size_t);
+      if(length < minDataSize)
+        return static_cast<size_t>(0);
+
+      void *readData = *reinterpret_cast<void **>(data);
+      size_t readDataLength = *reinterpret_cast<size_t *>(reinterpret_cast<uint8_t *>(data) + sizeof(void *));
+      send(readData, readDataLength);
+
+      return minDataSize;
+    }), "r", 1);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      const size_t minDataSize = sizeof(void *) + sizeof(size_t);
+      if(length < minDataSize)
+        return static_cast<size_t>(0);
+
+      void *writeDataDest = *reinterpret_cast<void **>(data);
+      size_t writeDataLength = *reinterpret_cast<size_t *>(reinterpret_cast<uint8_t *>(data) + sizeof(void *));
+      void *writeData = reinterpret_cast<void *>(reinterpret_cast<uint8_t *>(data) + sizeof(void *) + sizeof(size_t));
+
+      if(length < minDataSize + writeDataLength)
+        return static_cast<size_t>(0);
+
+      if(reinterpret_cast<size_t>(writeDataDest) >= 0x20000000 && reinterpret_cast<size_t>(writeDataDest) <= 0x2000FA00)
+      {
+        std::memcpy(writeDataDest, writeData, writeDataLength);
+      }
+      else if(reinterpret_cast<size_t>(writeDataDest) >= 0x8004000 && reinterpret_cast<size_t>(writeDataDest) <= 0x8008000)
+      {
+        FLASH_OBProgramInitTypeDef pOBInit;
+        pOBInit.OptionType = OPTIONBYTE_WRP;
+        pOBInit.WRPState = OB_WRPSTATE_DISABLE;
+        pOBInit.WRPSector = FLASH_SECTOR_1;
+        pOBInit.Banks = FLASH_BANK_1;
+        HAL_FLASHEx_OBProgram(&pOBInit);
+        if(reinterpret_cast<size_t>(writeDataDest) == 0x8004000)
+          FLASH_Erase_Sector(FLASH_SECTOR_1, VOLTAGE_RANGE_4);
+
+        for(size_t i = 0; i < (writeDataLength + sizeof(uint64_t) - 1) / sizeof(uint64_t); i++)
+        {
+          uint32_t programType = FLASH_TYPEPROGRAM_DOUBLEWORD;
+          HAL_FLASH_Program(programType, reinterpret_cast<size_t>(writeDataDest), reinterpret_cast<uint64_t *>(writeData)[i]);
+        }
+        pOBInit.WRPState = OB_WRPSTATE_ENABLE;
+        HAL_FLASHEx_OBProgram(&pOBInit);
+      }
+
+      char ack[1] = {6};
+      send(ack, sizeof(ack));
+
+      return minDataSize + writeDataLength;
+    }), "w", 1);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      if(_engineMain != 0)
+      {
+        const char responseText[19] = "Quiting EngineMain";
+        send((uint8_t*)responseText, strlen(responseText));
+        delete _engineMain;
+        _engineMain = 0;
+        send((uint8_t*)doneResponseText, strlen(doneResponseText));
+      }
+      else
+      {
+        const char responseText[25] = "EngineMain not running\n\r";
+        send((uint8_t*)responseText, strlen(responseText));
+      }
+      return static_cast<size_t>(0);
+    }), "q", 1, false);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      if(_engineMain == 0)
+      {
+        size_t configSize = 0;
+        const char responseText1[24] = "Initializing EngineMain";
+        send((uint8_t*)responseText1, strlen(responseText1));
+        _engineMain = new EFIGenieMain(&_config, configSize, &_embeddedIOServiceCollection, _variableMap);
+        _metadata = Config::OffsetConfig(&_config, configSize);
+        send((uint8_t*)doneResponseText, strlen(doneResponseText));
+
+        const char responseText2[22] = "Setting Up EngineMain";
+        send((uint8_t*)responseText2, strlen(responseText2));
+        _engineMain->Setup();
+        send((uint8_t*)doneResponseText, strlen(doneResponseText));
+      }
+      else
+      {
+        const char responseText[29] = "EngineMain already started\n\r";
+        send((uint8_t*)responseText, strlen(responseText));
+      }
+      return static_cast<size_t>(0);
+    }), "s", 1, false);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      if(length < sizeof(uint32_t))//make sure there are enough bytes to process a request
+        return static_cast<size_t>(0);
+      uint8_t offset = *reinterpret_cast<uint32_t *>(data); //grab offset from data
+
+      send(reinterpret_cast<const uint8_t *>(_metadata) + offset * 64, 64);
+
+      return static_cast<size_t>(sizeof(uint32_t));//return number of bytes handled
+    }), "m", 1);
+    _prefixHandler->RegisterReceiveCallBack(new communication_receive_callback_t([](communication_send_callback_t send, void *data, size_t length)
+    { 
+      size_t configLocation[1] = { reinterpret_cast<size_t>(&_config) };
+      send(configLocation, sizeof(configLocation));
+      
+      return static_cast<size_t>(0);
+    }), "c", 1, false);
 
     const char responseText5[24] = "Setting Up EngineMain\n\r";
     _cdcService->Send((uint8_t*)responseText5, strlen(responseText5));
@@ -81,7 +200,7 @@ extern "C"
     const char responseText6[19] = "EngineMain Setup\n\r";
     _cdcService->Send((uint8_t*)responseText6, strlen(responseText6));
     _cdcService->Flush();
-    loopTime = _engineMain->VariableMap->GenerateValue(250);
+    loopTime = _variableMap->GenerateValue(250);
   }
   void Loop() 
   {
