234bd6402526ed07f4e2ae2177fbc19851546039
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2024-04-02 00:26:57 -0500

    update EFIGenie

--- Core/Src/Entry.cpp
+++ Core/Src/Entry.cpp
@@ -10,7 +10,7 @@
 #include "Config.h"
 #include "CommunicationHandler_Prefix.h"
 #include "STM32HalCommunicationService_CDC.h"
-#include "CommunicationHandlers/CommunicationHandler_GetVariable.h"
+#include "CommunicationHandlers/CommunicationHandler_EFIGenie.h"
 #include "stm32f4xx_hal_flash_ex.h"
 
 using namespace OperationArchitecture;
@@ -24,8 +24,6 @@ extern char _config;
 
 extern "C"
 {
-  const char ack[1] = {6};
-  const char nack[1] = {21};
   GeneratorMap<Variable> *_variableMap;
   uint8_t VariableBuff[9];
   uint32_t Commands[32];
@@ -35,31 +33,76 @@ extern "C"
   bool secondCommand = false;
   EmbeddedIOServiceCollection _embeddedIOServiceCollection;
   STM32HalCommunicationService_CDC *_cdcService;
-  CommunicationHandler_GetVariable *_getVariableHandler;
-  CommunicationHandler_Prefix *_prefixHandler;
+  CommunicationHandler_EFIGenie *_efigenieHandler;
   EFIGenieMain *_engineMain;
   Variable *loopTime;
   uint32_t prev;
 
+  bool write(void *destination, const void *data, size_t length) {
+    if(reinterpret_cast<size_t>(destination) >= 0x20000000 && reinterpret_cast<size_t>(destination) <= 0x2000FA00)
+    {
+      std::memcpy(destination, data, length);
+    }
+    else if(reinterpret_cast<size_t>(destination) >= 0x8004000 && reinterpret_cast<size_t>(destination) <= 0x8008000)
+    {
+      HAL_FLASH_Unlock();
+      FLASH_OBProgramInitTypeDef pOBInit;
+      pOBInit.OptionType = OPTIONBYTE_WRP;
+      pOBInit.WRPState = OB_WRPSTATE_DISABLE;
+      pOBInit.WRPSector = FLASH_SECTOR_1;
+      pOBInit.Banks = FLASH_BANK_1;
+      HAL_FLASHEx_OBProgram(&pOBInit);
+      if(reinterpret_cast<size_t>(destination) == 0x8004000)
+      {
+        FLASH_EraseInitTypeDef pEraseInit;
+        pEraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
+        pEraseInit.NbSectors = 1;
+        pEraseInit.Sector = 1;
+        pEraseInit.VoltageRange = VOLTAGE_RANGE_3;
+
+        uint32_t SectorError;
+        HAL_FLASHEx_Erase(&pEraseInit, &SectorError);
+      }
+
+      for(size_t i = 0; i < length; i++)
+      {
+        uint32_t programType = FLASH_TYPEPROGRAM_BYTE;
+        uint8_t data = 0;
+        std::memcpy(&data,  reinterpret_cast<const void *>(reinterpret_cast<const uint8_t *>(data) + i), sizeof(uint8_t));
+        HAL_FLASH_Program(programType, reinterpret_cast<size_t>(destination) + i, data);
+      }
+      pOBInit.WRPState = OB_WRPSTATE_ENABLE;
+      HAL_FLASHEx_OBProgram(&pOBInit);
+      HAL_FLASH_Lock();
+    }
+    return true;
+  }
+
+  bool quit() {
+    if(_engineMain != 0)
+    {
+      delete _engineMain;
+      _engineMain = 0;
+    }
+    return true;
+  }
+
+  bool start() {
+    if(_engineMain == 0)
+    {
+      size_t configSize = 0;
+      _engineMain = new EFIGenieMain(&_config, configSize, &_embeddedIOServiceCollection, _variableMap);
+      _metadata = Config::OffsetConfig(&_config, *reinterpret_cast<const uint32_t *>(&_config) + 8);
+
+      _engineMain->Setup();
+    }
+    return true;
+  }
+
   void Setup() 
   {
-    //ignore the warnings. these are here so we can check the alignment so we can put it in the editor
-    volatile size_t uint8_align = alignof(uint8_t);
-    volatile size_t uint16_align = alignof(uint16_t);
-    volatile size_t uint32_align = alignof(uint32_t);
-    volatile size_t uint64_align = alignof(uint64_t);
-    volatile size_t bool_align = alignof(bool);
-    volatile size_t int8_align = alignof(int8_t);
-    volatile size_t int16_align = alignof(int16_t);
-    volatile size_t int32_align = alignof(int32_t);
-    volatile size_t int64_align = alignof(int64_t);
-    volatile size_t float_align = alignof(float);
-    volatile size_t double_align = alignof(double);
-
     _variableMap = new GeneratorMap<Variable>();
     _cdcService = new STM32HalCommunicationService_CDC();
-    _prefixHandler = new CommunicationHandler_Prefix();
-    _cdcService->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length){ return _prefixHandler->Receive(send, data, length);});
 
     _embeddedIOServiceCollection.DigitalService = new Stm32HalDigitalService();
     _embeddedIOServiceCollection.AnalogService = new Stm32HalAnalogService();
@@ -70,114 +113,9 @@ extern "C"
     _engineMain = new EFIGenieMain(reinterpret_cast<void*>(&_config), _configSize, &_embeddedIOServiceCollection, _variableMap);
 
     _metadata = Config::OffsetConfig(&_config, *reinterpret_cast<const uint32_t *>(&_config) + 8);
-    _getVariableHandler = new CommunicationHandler_GetVariable(_variableMap);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length){ return _getVariableHandler->Receive(send, data, length);}, "g", 1);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      const size_t minDataSize = sizeof(void *) + sizeof(size_t);
-      if(length < minDataSize)
-        return static_cast<size_t>(0);
 
-      const void *readData = *reinterpret_cast<void * const *>(data);
-      const size_t readDataLength = *reinterpret_cast<const size_t *>(reinterpret_cast<const uint8_t *>(data) + sizeof(void *));
-      send(readData, readDataLength);
-
-      return minDataSize;
-    }, "r", 1);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      const size_t minDataSize = sizeof(void *) + sizeof(size_t);
-      if(length < minDataSize)
-        return static_cast<size_t>(0);
-
-      void *writeDataDest = *reinterpret_cast<void * const *>(data);
-      const size_t writeDataLength = *reinterpret_cast<const size_t *>(reinterpret_cast<const uint8_t *>(data) + sizeof(void *));
-      const void *writeData = reinterpret_cast<const void *>(reinterpret_cast<const uint8_t *>(data) + sizeof(void *) + sizeof(size_t));
-
-      if(length < minDataSize + writeDataLength)
-        return static_cast<size_t>(0);
-
-      if(reinterpret_cast<size_t>(writeDataDest) >= 0x20000000 && reinterpret_cast<size_t>(writeDataDest) <= 0x2000FA00)
-      {
-        std::memcpy(writeDataDest, writeData, writeDataLength);
-      }
-      else if(reinterpret_cast<size_t>(writeDataDest) >= 0x8004000 && reinterpret_cast<size_t>(writeDataDest) <= 0x8008000)
-      {
-        HAL_FLASH_Unlock();
-        FLASH_OBProgramInitTypeDef pOBInit;
-        pOBInit.OptionType = OPTIONBYTE_WRP;
-        pOBInit.WRPState = OB_WRPSTATE_DISABLE;
-        pOBInit.WRPSector = FLASH_SECTOR_1;
-        pOBInit.Banks = FLASH_BANK_1;
-        HAL_FLASHEx_OBProgram(&pOBInit);
-        if(reinterpret_cast<size_t>(writeDataDest) == 0x8004000)
-        {
-          FLASH_EraseInitTypeDef pEraseInit;
-          pEraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
-          pEraseInit.NbSectors = 1;
-          pEraseInit.Sector = 1;
-          pEraseInit.VoltageRange = VOLTAGE_RANGE_3;
-
-          uint32_t SectorError;
-          HAL_FLASHEx_Erase(&pEraseInit, &SectorError);
-        }
-
-        for(size_t i = 0; i < writeDataLength; i++)
-        {
-          uint32_t programType = FLASH_TYPEPROGRAM_BYTE;
-          uint8_t data = 0;
-          std::memcpy(&data,  reinterpret_cast<const void *>(reinterpret_cast<const uint8_t *>(writeData) + i), sizeof(uint8_t));
-          HAL_FLASH_Program(programType, reinterpret_cast<size_t>(writeDataDest) + i, data);
-        }
-        pOBInit.WRPState = OB_WRPSTATE_ENABLE;
-        HAL_FLASHEx_OBProgram(&pOBInit);
-        HAL_FLASH_Lock();
-      }
-
-      send(ack, sizeof(ack));
-
-      return minDataSize + writeDataLength;
-    }, "w", 1);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      if(_engineMain != 0)
-      {
-        delete _engineMain;
-        _engineMain = 0;
-      }
-      send(ack, sizeof(ack));
-      return static_cast<size_t>(0);
-    }, "q", 1, false);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      if(_engineMain == 0)
-      {
-        size_t configSize = 0;
-        _engineMain = new EFIGenieMain(&_config, configSize, &_embeddedIOServiceCollection, _variableMap);
-        _metadata = Config::OffsetConfig(&_config, *reinterpret_cast<const uint32_t *>(&_config) + 8);
-
-        _engineMain->Setup();
-      }
-      send(ack, sizeof(ack));
-      return static_cast<size_t>(0);
-    }, "s", 1, false);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      if(length < sizeof(uint32_t))//make sure there are enough bytes to process a request
-        return static_cast<size_t>(0);
-      const uint8_t offset = *reinterpret_cast<const uint32_t *>(data); //grab offset from data
-
-      send(reinterpret_cast<const uint8_t *>(_metadata) + offset * 64, 64);
-
-      return static_cast<size_t>(sizeof(uint32_t));//return number of bytes handled
-    }, "m", 1);
-    _prefixHandler->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length)
-    { 
-      size_t configLocation[1] = { reinterpret_cast<size_t>(&_config) };
-      send(configLocation, sizeof(configLocation));
-      
-      return static_cast<size_t>(0);
-    }, "c", 1, false);
+    _efigenieHandler = new CommunicationHandler_EFIGenie(_variableMap, write, quit, start, reinterpret_cast<void*>(&_config), _metadata);
+    _cdcService->RegisterReceiveCallBack([](communication_send_callback_t send, const void *data, size_t length){ return _efigenieHandler->Receive(send, data, length);});
 
     _engineMain->Setup();
     loopTime = _variableMap->GenerateValue(250);
@@ -185,7 +123,7 @@ extern "C"
   void Loop() 
   {
     const tick_t now = _embeddedIOServiceCollection.TimerService->GetTick();
-    loopTime->Set((float)(now-prev) / _embeddedIOServiceCollection.TimerService->GetTicksPerSecond());
+    *loopTime = (float)(now-prev) / _embeddedIOServiceCollection.TimerService->GetTicksPerSecond();
     prev = now;
     _cdcService->Flush();
 
